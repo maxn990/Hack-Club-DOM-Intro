ac469fe928f4e8037b28ec7b80c584cd4eab355e
m990 <maxn990@gmail.com>
2021-01-11 21:03:50 -0800

    init - uploaded template
    1] Template with blank/commented main.js file
    
    2] All other files are filled out completely

---
+++ js/main.js
@@ -0,0 +1,47 @@
+/*
+
+This is the JS file we'll be working out of. We'll be building a website that
+will allow you to build custom cards for any occasion, including birthdays.
+The user will enter the URL of the image they want to use as a background, their
+name, and what text they want on the card.
+
+To accomplish this, we'll edit what's called the Document Object Model (DOM).
+The DOM has access to every part of a html tag.
+
+<h1 style="background-color: red">Hello</h1>
+
+In the tag above we can see that there are 2 parts, text and style. We can edit
+elements of a tag using a litney of commands. We'll start with text.
+
+element.innerHTML = Hello World!;
+
+innerHTMl changes the text. Our tag now looks like the following -
+<h1 style="background-color: red">Hello !orld!</h1>
+
+Here's some more examples from W3 schools:
+Property	                                 Description
+element.innerHTML =  new html content	     Change the inner HTML of an element
+element.attribute = new value	Change       the attribute value of an HTML element
+element.style.property = new style	       Change the style of an HTML element
+element.setAttribute(attribute, value)	   Change the attribute value of an HTML element
+
+For more information, read this entry on W3 schools
+https://www.w3schools.com/js/js_htmldom.asp
+*/
+
+
+// First, let's use the DOM to connect our button to javascript.
+
+// This is the function that will run when we submit
+function editCard(){
+  // First, lets get the user input and save it to variables. This also uses DOM
+
+
+  // The second and final step is to change the values in our card.
+
+
+  // Now that we've changed those values, we want to change the background
+  // by editing the style of our element. In this case, we want to change the
+  // style.
+
+}
